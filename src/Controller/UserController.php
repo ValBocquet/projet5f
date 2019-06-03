1c31ed11ae2ad59ee89b4f5ebc4cbf1306831440
ValBocquet <valentin.bocquet9@gmail.com>
2019-06-03 19:05:25 +0200

    ajout modif mdp mais erreur soumission des deux forms

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,10 +7,14 @@ use App\Repository\UsersRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Class UserController
@@ -21,29 +25,70 @@ class UserController extends AbstractController
     /**
      * @Route("/user", name="user")
      */
-    public function index(EntityManagerInterface $entityManager, Request $request, UsersRepository $repository) : Response {
+    public function index(EntityManagerInterface $entityManager, Request $request, UsersRepository $repository, UserPasswordEncoderInterface $encoder) : Response {
         $form = $this->createFormBuilder()
             ->add('avatar_img', FileType::class)
             ->getForm();
         $form->handleRequest($request);
 
-        $user = $this->getUser();
+
 
         if ($form->isSubmitted() && $form->isValid()) {
-            if($user->getAvatar() !== null) {
-                /* on peut ajouter un avatar */
-                $file = $user->getAvatar();
-                $user->setAvatar($file);
-            }
 
+
+            $user = $this->getUser();
+
+            $file = $form->get('avatar_img')->getData();
+            $fileName = md5(uniqid()).'.'.$file->guessExtension();
+            $file->move($this->getParameter('upload_directory'), $fileName);
+
+
+            // Move the file to the directory where brochures are stored
+
+            $user->setAvatar($fileName);
+            /* on peut ajouter un avatar */
+
+            $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
 
             return $this->redirectToRoute('home');
 
         }
+
+        $formPass = $this->createFormBuilder()
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent être identiques.',
+                'options' => ['attr' => ['class' => 'form-control']],
+                'required' => true,
+                'first_options' => ['label' => 'Mot de passe :'],
+                'second_options' => ['label' => 'Confirmez le mot de passe :'],
+            ])
+            ->getForm();
+
+        $formPass->handleRequest($request);
+
+        if($formPass->isSubmitted() && $formPass->isValid()) {
+            $user = $this->getUser();
+            $pass = $formPass->get('password')->getData();
+
+
+            $hash = $encoder->encodePassword($user, $pass);
+
+            $user->setPassword($hash);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
+
         return $this->render('user/index.html.twig', [
             'form' => $form->createView(),
+            'formPass' => $formPass->createView()
         ]);
     }
 
